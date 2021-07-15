3a99bfe702578261c79dbbb8d89299ad15f014ff
bepasquet <plescanopasquet@gmail.com>
2021-07-15 10:04:43 +0700

    - documentation added to combine reducers - combine reducers refactor

--- libs/from-reducer/src/lib/util/combine-reducers.ts
+++ libs/from-reducer/src/lib/util/combine-reducers.ts
@@ -1,16 +1,68 @@
 import { Reducer } from '../types';
 
+/**
+ * Given a dictionary of reducer functions creates a single reducer that once applied
+ * will call each reducer with its corresponding value of the state as its first argument
+ * and the event as its second.
+ * @param reducers a dictionary (object) of key (place to store result of the reduction), value (reducer function to apply against current state and event)
+ * @returns single reducer function, this may be useful when we want to pass an event as argument to multiple reducers
+ *
+ * @example
+ *import { Action } from 'from-reducer';
+ *
+ * interface UsersState {
+ *   auth: boolean;
+ * }
+ *
+ * const userReducer = (state: UsersState, action: Action) => {
+ *   switch (action.type) {
+ *     case 'AUTHENTICATE': {
+ *       return { auth: true };
+ *     }
+ *
+ *     default: {
+ *       return state;
+ *     }
+ *   }
+ * };
+ *
+ * interface ProductsState {
+ *   show: boolean;
+ * }
+ *
+ * const productsReducer = (state: ProductsState, action: Action) => {
+ *   switch (action.type) {
+ *     case 'TOGGLE_PRODUCTS': {
+ *       return { ...state, show: !state.show };
+ *     }
+ *
+ *     default: {
+ *       return state;
+ *     }
+ *   }
+ * };
+ *
+ * const reducers = {
+ *   users: userReducer,
+ *   products: productsReducer,
+ * };
+ *
+ * const reducer = combineReducers(reducers);
+ *
+ */
 export function combineReducers<
   S extends Record<K, S[K]>,
   T,
   K extends keyof S
 >(reducers: Record<K, Reducer<S[K], T>>): Reducer<S, T> {
-  return (gState: S, value: T) =>
-    Object.entries<Reducer<S[K], T>>(reducers).reduce(
-      (state, [key, reducer]) => ({
-        ...state,
-        [key as K]: reducer(state[key as K], value),
+  const rs = Object.entries<Reducer<S[K], T>>(reducers);
+
+  return (state: S, event: T) =>
+    rs.reduce(
+      (s, [key, reducer]) => ({
+        ...s,
+        [key]: reducer(s[key], event),
       }),
-      gState
+      state
     );
 }
--- libs/from-reducer/src/lib/util/reducer-compare.ts
+++
@@ -1,3 +0,0 @@
-export function distinctStateCompare<S>(prev: S, curr: S): boolean {
-  return JSON.stringify(prev) === JSON.stringify(curr);
-}
