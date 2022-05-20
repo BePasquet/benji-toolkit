bb322959b56599bcdf1aa30208a712dd520f76be
bepasquet <plescanopasquet@gmail.com>
2022-05-20 19:22:33 +0700

    - reactive actor react bindings publishable

--- apps/from-reducer-react-global-example/src/app/users/users.component.tsx
+++ apps/from-reducer-react-global-example/src/app/users/users.component.tsx
@@ -9,7 +9,6 @@ import React, { useEffect } from 'react';
 export const Users = () => {
   const dispatch = useDispatch();
   const state = useSelector((x) => x);
-  console.log(state);
 
   useEffect(() => {
     dispatch(getUsers());
