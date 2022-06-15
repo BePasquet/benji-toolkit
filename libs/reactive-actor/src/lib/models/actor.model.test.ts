b6631cc64f36646886f584dcd1a62c0b446136aa
bepasquet <plescanopasquet@gmail.com>
2022-06-15 19:53:51 +0700

    - actor model unit test added

--- libs/reactive-actor/src/lib/models/actor.model.test.ts
+++ libs/reactive-actor/src/lib/models/actor.model.test.ts
@@ -3,10 +3,12 @@ import { map } from 'rxjs';
 import { TestScheduler } from 'rxjs/testing';
 import { addRecipient } from '../operators';
 import { createEvent } from '../util';
-import { Actor } from './actor.model';
+import { Actor, stop } from './actor.model';
 
 const startEvent = createEvent('START');
 const pauseEvent = createEvent('PAUSE');
+const replayEvent = createEvent('REPLAY');
+
 describe('Actor Model', () => {
   let testScheduler: TestScheduler;
 
@@ -40,24 +42,25 @@ describe('Actor Model', () => {
     testScheduler.run(({ expectObservable, cold }) => {
       const actor = new Actor('unknown');
 
-      const firstEvent = startEvent(null);
-      const secondEvent = pauseEvent(null);
-      const thirdEvent = startEvent(null);
+      const start = startEvent(null);
+      const pause = pauseEvent(null);
+      const replay = replayEvent(null);
 
       const firstEventTime = '-a';
       const secondEventTime = '--b';
       const thirdEventTime = '---c';
+      const expected = '-abc';
 
-      cold(firstEventTime).subscribe(() => actor.send(firstEvent));
+      cold(firstEventTime).subscribe(() => actor.send(start));
 
-      cold(secondEventTime).subscribe(() => actor.send(secondEvent));
+      cold(secondEventTime).subscribe(() => actor.send(pause));
 
-      cold(thirdEventTime).subscribe(() => actor.send(thirdEvent));
+      cold(thirdEventTime).subscribe(() => actor.send(replay));
 
-      expectObservable((actor as any).messages$).toBe('-abc', {
-        a: firstEvent,
-        b: secondEvent,
-        c: thirdEvent,
+      expectObservable((actor as any).messages$).toBe(expected, {
+        a: start,
+        b: pause,
+        c: replay,
       });
     });
   });
@@ -66,30 +69,34 @@ describe('Actor Model', () => {
     testScheduler.run(({ expectObservable, cold }) => {
       const actor = new Actor('unknown');
 
-      const firstEvent = startEvent(null);
-      const expectedEvent = pauseEvent(null);
-      const expected = '-b';
+      const start = startEvent(null);
+      const pause = pauseEvent(null);
+      const expected = '-(ba)';
+
       const message$ = (actor as any)?.messages$.pipe(
         ofType(startEvent),
-        map(() => expectedEvent)
+        map(() => pause)
       );
 
       (actor as any).answer(message$);
 
-      cold('-a').subscribe(() => actor.send(firstEvent));
+      cold('-a').subscribe(() => actor.send(start));
 
-      expectObservable(message$).toBe(expected, { b: expectedEvent });
+      expectObservable((actor as any).messages$).toBe(expected, {
+        a: start,
+        b: pause,
+      });
     });
   });
 
   it('Should send messages to others', () => {
     testScheduler.run(({ expectObservable, cold, flush }) => {
-      const actorSender = new Actor('a');
-      const actorRecipient = new Actor('b');
+      const actorSender = new Actor('unknownSender');
+      const actorRecipient = new Actor('unknownReceiver');
 
       jest.spyOn(actorRecipient, 'send');
 
-      const firstEvent = startEvent(null);
+      const start = startEvent(null);
       const expected = '-a';
 
       const message$ = (actorSender as any)?.messages$.pipe(
@@ -99,15 +106,68 @@ describe('Actor Model', () => {
 
       (actorSender as any).answer(message$);
 
-      cold('-a').subscribe(() => actorSender.send(firstEvent));
+      cold('-a').subscribe(() => actorSender.send(start));
 
       expectObservable(message$).toBe(expected, {
-        a: { ...firstEvent, recipient: actorRecipient },
+        a: { ...start, recipient: actorRecipient },
       });
 
       flush();
 
       expect(actorRecipient.send).toHaveBeenCalled();
+    });
+  });
+
+  it('Should execute behavior every time matches a particular message', () => {
+    testScheduler.run(({ expectObservable, cold }) => {
+      const actor = new Actor('unknown');
+
+      const start = startEvent(null);
+      const pause = pauseEvent(null);
+
+      cold('-a').subscribe(() => actor.send(start));
+      cold('------b').subscribe(() => actor.send(start));
+
+      const message$ = (actor as any)?.messages$.pipe(
+        ofType(startEvent),
+        map(() => pause)
+      );
+
+      (actor as any)?.answer(message$);
+
+      expectObservable((actor as any).messages$).toBe('-(ba)-(ba)', {
+        a: start,
+        b: pause,
+      });
+    });
+  });
+
+  it('Should not execute behavior after stop message has been sent', () => {
+    testScheduler.run(({ expectObservable, cold }) => {
+      const actor = new Actor('unknown');
+
+      const start = startEvent(null);
+      const pause = pauseEvent(null);
+      const stopEv = stop(null);
+      const expected = '-(ba)-saa';
+
+      cold('-a').subscribe(() => actor.send(start));
+      cold('------s').subscribe(() => actor.send(stopEv));
+      cold('-------a').subscribe(() => actor.send(start));
+      cold('--------a').subscribe(() => actor.send(start));
+
+      const message$ = (actor as any)?.messages$.pipe(
+        ofType(startEvent),
+        map(() => pause)
+      );
+
+      (actor as any)?.answer(message$);
+
+      expectObservable((actor as any).messages$).toBe(expected, {
+        a: start,
+        b: pause,
+        s: stopEv,
+      });
     });
   });
 });
