29d6f7b0a69322f7793b9f055967590e87d0734d
Pedro Benjamin Lescano Pasquet <plescanopasquet@gmail.com>
2022-09-29 18:03:35 +0700

    - event typings fix

--- libs/reactive-actor/src/lib/interfaces/event.interface.ts
+++ libs/reactive-actor/src/lib/interfaces/event.interface.ts
@@ -2,6 +2,7 @@
  * Message are implemented with a type
  * to understand what message is
  */
-export interface Event {
+export interface Event<T = any> {
   type: string;
+  payload: T;
 }
--- libs/reactive-actor/src/lib/interfaces/synthetic-event.interface.ts
+++ libs/reactive-actor/src/lib/interfaces/synthetic-event.interface.ts
@@ -1,5 +1,6 @@
 import { ActorEvent } from '../types/actor-event.type';
 import { ActorRef } from './actor-ref.interface';
+import { Event } from './event.interface';
 
 export interface SyntheticEvent<T = unknown, SE extends ActorEvent = Event> {
   type: string;
