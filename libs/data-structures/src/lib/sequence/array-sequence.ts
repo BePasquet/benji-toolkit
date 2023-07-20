d1a7e36227935dd3d3e8c6e53e29bcba6fa740f9
Pedro Benjamin Lescano Pasquet <plescanopasquet@gmail.com>
2023-07-20 08:04:54 +0700

    - array sequence time complexity added

--- libs/data-structures/src/lib/sequence/array-sequence.ts
+++ libs/data-structures/src/lib/sequence/array-sequence.ts
@@ -1,5 +1,8 @@
 import { Sequence } from './sequence.interface';
 
+/**
+ * Static array implementation of a sequence
+ */
 export class ArraySequence implements Sequence<number> {
   private data = new Int8Array(0);
 
@@ -9,6 +12,11 @@ export class ArraySequence implements Sequence<number> {
    */
   private size = 0;
 
+  /**
+   * @inheritdoc
+   * Time complexity O(n), every time it builds the
+   * sequence needs to go through all the elements and copy them
+   */
   build(elements: Iterable<number>): void {
     let iterableSize = 0;
 
@@ -22,18 +30,19 @@ export class ArraySequence implements Sequence<number> {
 
     const iterator = elements[Symbol.iterator]();
 
-    let i = 0;
-    let current = iterator.next();
-
-    while (i < iterableSize || !current.done) {
-      this.data[i] = current.value;
-      current = iterator.next();
-      i++;
+    for (let i = 0; i < iterableSize; i++) {
+      this.data[i] = iterator.next().value;
     }
 
     this.size = iterableSize;
   }
 
+  /**
+   * @inheritdoc
+   * Time Complexity O(1), When accessing an array by index this will
+   * look where it is allocated in memory and multiply the word length and the index (offset)
+   * to find the starting position of the element `address + w-bit * index`
+   */
   getAt(index: number): number | null {
     if (this.isIndexInRange(index)) {
       return null;
@@ -42,6 +51,12 @@ export class ArraySequence implements Sequence<number> {
     return this.data[index];
   }
 
+  /**
+   * @inheritdoc
+   * Time Complexity O(1), When accessing an array by index this will
+   * look where it is allocated in memory and multiply the word length and the index (offset)
+   * to find the starting position of the element `address + w-bit * index` to overwrite it
+   */
   setAt(index: number, value: number): void {
     if (this.isIndexInRange(index)) {
       return;
@@ -49,7 +64,11 @@ export class ArraySequence implements Sequence<number> {
 
     this.data[index] = value;
   }
-
+  /**
+   * @inheritdoc
+   * Time complexity O(n), An array allocate memory contiguously with a predetermined size,
+   * to add an element we need to resize by allocating a bigger space in memory and copying elements over
+   */
   insertAt(index: number, element: number): void {
     if (this.isIndexInRange(index)) {
       return;
@@ -73,6 +92,11 @@ export class ArraySequence implements Sequence<number> {
     this.data = newData;
   }
 
+  /**
+   * @inheritdoc
+   * Time complexity O(n), deleting an element will result in re indexing the elements at its right
+   * in memory in the worst case will delete the first element and need to shift n - 1 elements by one position
+   */
   deleteAt(index: number): number | null {
     if (this.isIndexInRange(index)) {
       return null;
@@ -96,18 +120,35 @@ export class ArraySequence implements Sequence<number> {
     return element;
   }
 
+  /**
+   * @inheritdoc
+   * Time complexity O(n), deleting the first element will result in re indexing the elements at its right
+   * in memory, shifting n - 1 elements by one position
+   */
   deleteFirst(): number | null {
     return this.deleteAt(0);
   }
 
+  /**
+   * @inheritdoc
+   * Time complexity 0(n), will need to resize array and copy elements over
+   */
   deleteLast(): number | null {
     return this.deleteAt(this.size - 1);
   }
 
+  /**
+   * @inheritdoc
+   * Time complexity 0(n), will need to resize the array and copy elements over
+   */
   insertFirst(element: number): void {
     this.insertAt(0, element);
   }
 
+  /**
+   * @inheritdoc
+   * Time complexity 0(n), will need to resize array and copy elements over
+   */
   insertLast(element: number): void {
     this.size++;
     const newData = new Int8Array(this.size);
@@ -144,6 +185,12 @@ export class ArraySequence implements Sequence<number> {
     };
   }
 
+  /**
+   * Checks whether the index is withing the bounds of the array
+   * 0 <= index < length
+   * @param index we want to check
+   * @returns whether is within the array bounds
+   */
   private isIndexInRange(index: number): boolean {
     return index >= this.size || index < 0;
   }
